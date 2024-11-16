2cfd5eb0f194b2604eb573e4d5c085a506c97463
Diana <dianavargasf@hotmail.es>
2024-11-16 14:48:00 +0700

    Initial commit: Project setup

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import AgentDirectory from "../components/AgentDirectory";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen p-4">
+      <AgentDirectory />
+    </main>
+  );
+}
